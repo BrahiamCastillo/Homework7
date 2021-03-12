2a66feda8ca34d30d0e25b0c32e9458e0342a63e
felix281202 <felix_infanteramirez28@yahoo.com>
2021-03-11 20:22:25 -0400

    parte de consulta a la base de datos y mostrar los datos

---
+++ DatosAPI.cs
@@ -0,0 +1,11 @@
+namespace Homework7
+{
+    public class DatosAPI
+    {
+        public string Nombres {get; set;}
+
+        public string Apellido1 {get; set;}
+
+        public string  Apellido2 {get; set;}
+    }
+}
